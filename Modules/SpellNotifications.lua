1b60cc30f7037a34543de67e2b100ecf394bd90d
clicketz <40314230+clicketz@users.noreply.github.com>
2024-05-20 14:37:49 -0600

    SPELL_DAMAGE event unnecessary

--- Modules/SpellNotifications.lua
+++ Modules/SpellNotifications.lua
@@ -39,32 +39,30 @@ local ACTION_TYPE = {
 local f = CreateFrame("Frame")
 f:RegisterEvent("PLAYER_LOGIN")
 f:SetScript("OnEvent", function(self, event, ...)
-	local timestamp, eventType, hideCaster, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags, extraArg1, extraArg2, extraArg3, extraArg4, extraArg5, extraArg6, extraArg7, extraArg8, extraArg9, extraArg10 = CombatLogGetCurrentEventInfo()
+    local timestamp, eventType, hideCaster, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags, extraArg1, extraArg2, extraArg3, extraArg4, extraArg5, extraArg6, extraArg7, extraArg8, extraArg9, extraArg10 = CombatLogGetCurrentEventInfo()
 
-	if event == "COMBAT_LOG_EVENT_UNFILTERED" then
-        -- Fade / Immunities / Reflects / Etc
+    if event == "COMBAT_LOG_EVENT_UNFILTERED" then
         if eventType == "SPELL_MISSED" then
+            -- Fade / Immunities / Reflects / Etc
             if destGUID == playerGUID then
                 if MISS_TYPE_DEST[extraArg4] then
                     local icon = select(3, GetSpellInfo(extraArg1)) or defaultIcon
-                    UIErrorsFrame:AddMessage(format("Self->"..extraArg4..": ".."|T"..icon..":0|t".."%s", GetSpellLink(extraArg1)))
+                    UIErrorsFrame:AddMessage(format("Self->" .. extraArg4 .. ": " .. "|T" .. icon .. ":0|t" .. "%s", GetSpellLink(extraArg1)))
                 end
             elseif sourceGUID == playerGUID then
                 if MISS_TYPE_SOURCE[extraArg4] then
                     local icon = select(3, GetSpellInfo(extraArg1)) or defaultIcon
-                    UIErrorsFrame:AddMessage(format(extraArg4..": ".."|T"..icon..":0|t".."%s", GetSpellLink(extraArg1)))
+                    UIErrorsFrame:AddMessage(format(extraArg4 .. ": " .. "|T" .. icon .. ":0|t" .. "%s", GetSpellLink(extraArg1)))
                 end
             end
-        end
 
-        if eventType == "SPELL_DAMAGE" then
             -- Grounding Totem
             if destName == groundingTotemNameLocalized
-                and bit_band(destFlags, COMBATLOG_OBJECT_AFFILIATION_MINE) > 0
-                and bit_band(destFlags, COMBATLOG_OBJECT_TYPE_GUARDIAN) > 0
-                and bit_band(sourceFlags, COMBATLOG_OBJECT_REACTION_HOSTILE) > 0 then
-                    local icon = select(3, GetSpellInfo(extraArg1)) or defaultIcon
-                    UIErrorsFrame:AddMessage(format("Ground: ".."|T"..icon..":0|t".."%s", GetSpellLink(extraArg1)))
+            and bit_band(destFlags, COMBATLOG_OBJECT_AFFILIATION_MINE) > 0
+            and bit_band(destFlags, COMBATLOG_OBJECT_TYPE_GUARDIAN) > 0
+            and bit_band(sourceFlags, COMBATLOG_OBJECT_REACTION_HOSTILE) > 0 then
+                local icon = select(3, GetSpellInfo(extraArg1)) or defaultIcon
+                UIErrorsFrame:AddMessage(format("Ground: " .. "|T" .. icon .. ":0|t" .. "%s", GetSpellLink(extraArg1)))
             end
         end
 
@@ -75,7 +73,7 @@ f:SetScript("OnEvent", function(self, event, ...)
             and sourceGUID == playerGUID
             then
                 local icon = select(3, GetSpellInfo(extraArg1)) or defaultIcon
-                UIErrorsFrame:AddMessage(format("Deathed: ".."|T"..icon..":0|t".."%s", GetSpellLink(extraArg1)))
+                UIErrorsFrame:AddMessage(format("Deathed: " .. "|T" .. icon .. ":0|t" .. "%s", GetSpellLink(extraArg1)))
             end
         end
 
@@ -85,14 +83,14 @@ f:SetScript("OnEvent", function(self, event, ...)
 
             if action then
                 local icon = select(3, GetSpellInfo(extraArg4)) or defaultIcon
-                UIErrorsFrame:AddMessage(format("%s: ".."|T"..icon..":0|t".."%s", action, GetSpellInfo(extraArg4)))
+                UIErrorsFrame:AddMessage(format("%s: " .. "|T" .. icon .. ":0|t" .. "%s", action, GetSpellInfo(extraArg4)))
             end
         end
-	elseif event == "PLAYER_LOGIN" then
+    elseif event == "PLAYER_LOGIN" then
         playerGUID = UnitGUID("player")
         defaultIcon = select(3, GetSpellInfo(5024)) -- Chicken Icon
         groundingTotemNameLocalized = GetSpellInfo(204336)
 
         self:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-	end
+    end
 end)
