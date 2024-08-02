c9efe9f6ae2ce54569f3e86901279803557493fb
clicketz <40314230+clicketz@users.noreply.github.com>
2024-08-01 18:34:27 -0600

    Hide more red frames on LossOfControl frame

--- Modules/LossOfControl.lua
+++ Modules/LossOfControl.lua
@@ -1,27 +1,27 @@
 local cf = CreateFrame("Frame")
 cf:RegisterEvent("PLAYER_LOGIN")
 cf:SetScript("OnEvent", function(self, event)
+    LossOfControlFrame:SetScale(.7)
+    --Center Icon
+    select(4, LossOfControlFrame:GetRegions()):ClearAllPoints()
+    select(4, LossOfControlFrame:GetRegions()):SetPoint("CENTER", 0, 0)
+    select(4, LossOfControlFrame:GetRegions()).SetPoint = function() end
+    --Background Shadow
+    select(1, LossOfControlFrame:GetRegions()):SetAlpha(0)
+    --Red Frame
+    select(2, LossOfControlFrame:GetRegions()):SetAlpha(0)
+    select(3, LossOfControlFrame:GetRegions()):SetAlpha(0)
+    LossOfControlFrame.RedLineBottom:SetAlpha(0)
+    LossOfControlFrame.RedLineTop:SetAlpha(0)
+    --Effect Text
+    select(5, LossOfControlFrame:GetRegions()):ClearAllPoints()
+    select(5, LossOfControlFrame:GetRegions()):SetPoint("TOP", select(4, LossOfControlFrame:GetRegions()), "BOTTOM")
+    --Countdown Text
+    select(2, LossOfControlFrame:GetChildren()):SetAlpha(0)
 
-	LossOfControlFrame:SetScale(.7)
-	--Center Icon
-	select(4,LossOfControlFrame:GetRegions()):ClearAllPoints()
-	select(4,LossOfControlFrame:GetRegions()):SetPoint("CENTER",0,0)
-	select(4,LossOfControlFrame:GetRegions()).SetPoint = function() end
-	--Background Shadow
-	select(1,LossOfControlFrame:GetRegions()):SetAlpha(0)
-	--Red Frame
-	select(2,LossOfControlFrame:GetRegions()):SetAlpha(0)
-	select(3,LossOfControlFrame:GetRegions()):SetAlpha(0)
-	--Effect Text
-	select(5,LossOfControlFrame:GetRegions()):ClearAllPoints()
-	select(5,LossOfControlFrame:GetRegions()):SetPoint("TOP",select(4,LossOfControlFrame:GetRegions()),"BOTTOM")
-	--Countdown Text
-	select(2,LossOfControlFrame:GetChildren()):SetAlpha(0)
-
-	-- LossOfControlFrame:ClearAllPoints()
-	-- LossOfControlFrame:SetPoint("CENTER", UIParent, "CENTER", 0, 0)
-	-- select(1, LossOfControlFrame:GetRegions()):SetAlpha(0)
-	-- select(2, LossOfControlFrame:GetRegions()):SetAlpha(0)
-	-- select(3, LossOfControlFrame:GetRegions()):SetAlpha(0)
-
+    -- LossOfControlFrame:ClearAllPoints()
+    -- LossOfControlFrame:SetPoint("CENTER", UIParent, "CENTER", 0, 0)
+    -- select(1, LossOfControlFrame:GetRegions()):SetAlpha(0)
+    -- select(2, LossOfControlFrame:GetRegions()):SetAlpha(0)
+    -- select(3, LossOfControlFrame:GetRegions()):SetAlpha(0)
 end)
